babe9f58536f5f90e403f316fd49dd652117c386
Pakapon Kaewmark <sleep191@gmail.com>
2017-05-23 03:14:37 +0700

    Add hasMany Comment Relationship to Photo Model

--- app/Photo.php
+++ app/Photo.php
@@ -44,4 +44,8 @@ class Photo extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function comments(){
+        return $this->hasMany('App\Comment');
+    }
 }

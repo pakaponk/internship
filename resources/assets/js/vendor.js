e8a11f1ddeef212d583a818473233a44a568fce0
Pakapon Kaewmark <sleep191@gmail.com>
2017-05-22 19:33:50 +0700

    Add NPM Dependency for Sweet Alert

--- resources/assets/js/vendor.js
+++ resources/assets/js/vendor.js
@@ -2,4 +2,6 @@
 
 import angular from 'angular'
 import uiRouter from '@uirouter/angularjs'
+import swal from 'sweetalert'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'sweetalert/dist/sweetalert.css'

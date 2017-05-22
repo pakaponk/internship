504be4b29fee95bfe9ee67a7fed5b81642c925e3
Pakapon Kaewmark <sleep191@gmail.com>
2017-05-23 00:20:55 +0700

    Add Mutator for `source` field inside Photo Model - This method will save image at the local storage and store the address of the image to `source` field if the given value is Image

--- app/Photo.php
+++ app/Photo.php
@@ -17,4 +17,27 @@ class Photo extends Model
     protected $fillable = [
         'source', 'caption'
     ];
+
+    public function setSourceAttribute($value){
+        switch(gettype($value))
+        {
+            case "object":
+                $photo = Image::make($value);
+                $photoDirectoryPath = public_path('images/photos');
+                if (!is_dir($photoDirectoryPath)){
+                    mkdir($photoDirectoryPath, 0755);
+                }  
+                
+                $photoRelativePath = 'images/photos/' . $this->attributes['id'] . '.jpg';
+                $photo->save(public_path($photoRelativePath, 100));
+
+                $this->attributes['source'] = $photoRelativePath;
+                break;
+            case "string":
+                $this->attributes['source'] = $value;
+                break;
+            default:
+                break;
+        }
+    }
 }
